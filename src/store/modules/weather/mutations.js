f73bafc6594ce5bf1825f2dfa533c0d64f2e2c74
Bódi <axel3425@hotmail.com>
2019-02-05 22:21:21 +0100

    Weather-tile version 0.8.3a Wind speed number formatting

--- src/store/modules/weather/mutations.js
+++ src/store/modules/weather/mutations.js
@@ -8,7 +8,7 @@ const mutations= {
     state.today.one.time=now.hour()+":00";
     state.today.one.temp=Math.round(payload.main.temp);
     state.today.one.cond=codeConverter(payload.weather[0].icon);
-    state.today.one.wind=payload.wind.speed*3.6;
+    state.today.one.wind= Math.trunc(payload.wind.speed*3.6);
     state.today.one.description=payload.weather[0].description;
     state.today.one.humidity=payload.main.humidity;
 
@@ -19,7 +19,7 @@ const mutations= {
     state.today.two.time=now.hour()+":00";
     state.today.two.temp=Math.round(payload[0].main.temp);
     state.today.two.cond=codeConverter(payload[0].weather[0].icon);
-    state.today.two.wind=payload[0].wind.speed*3.6;
+    state.today.two.wind=Math.trunc(payload[0].wind.speed*3.6);
     state.today.two.description=payload[0].weather[0].description;
     state.today.two.humidity=payload[0].main.humidity;
 
@@ -27,7 +27,7 @@ const mutations= {
     state.today.three.time=now.hour()+":00";
     state.today.three.temp=Math.round(payload[1].main.temp);
     state.today.three.cond=codeConverter(payload[1].weather[0].icon);
-    state.today.three.wind=payload[0].wind.speed*3.6;
+    state.today.three.wind=Math.trunc(payload[0].wind.speed*3.6);
     state.today.three.description=payload[1].weather[0].description
     state.today.three.humidity=payload[1].main.humidity;
  },

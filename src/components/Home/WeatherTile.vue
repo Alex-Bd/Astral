0dc0bfa1d046c043842cde2738023c5471f3a8a9
Bódi <axel3425@hotmail.com>
2019-02-07 16:20:33 +0100

    Weather-tile version 0.9.2d Template code format and typo fix

--- src/components/Home/WeatherTile.vue
+++ src/components/Home/WeatherTile.vue
@@ -1,39 +1,37 @@
 <template>
   <div class="doc-container">
-<div class="slot" ref="scroll" v-on:wheel="horiontalScrolling" >
-    <div  class="row items-center scroll">
+    <div class="slot" ref="scroll" v-on:wheel="horizontalScrolling" >
+      <div  class="row items-center scroll">
+        <div v-for="weather in weather" :key="weather.id">
 
+          <div class="col day-slot">
 
-
-      <div v-for="weather in weather" :key="weather.id">
-
-       <div class="col day-slot">
-          <div class="row">
-            <div class="col">
-              <div align="center">{{weather.time}}</div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div align="center" class="col">
-              <q-icon :name="weather.cond"  color="white" size="8vh"></q-icon>
-              <span class="temp">{{weather.temp}}<q-icon name= "fas fa-temperature-low" color="white" size="3vh"></q-icon></span>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col">
-              <div align="center">{{weather.description}}</div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col">
-              <div align="center">
-                <q-icon name="fas fa-wind"  color="white" size="3vh"></q-icon>
-                {{weather.wind}}
+            <div class="row">
+              <div class="col">
+                <div align="center">{{weather.time}}</div>
               </div>
             </div>
+
+            <div class="row">
+              <div align="center" class="col">
+                <q-icon :name="weather.cond"  color="white" size="8vh"></q-icon>
+                <span class="temp">{{weather.temp}}<q-icon name= "fas fa-temperature-low" color="white" size="3vh"></q-icon></span>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+                <div align="center">{{weather.description}}</div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+                <div align="center">
+                  <q-icon name="fas fa-wind"  color="white" size="3vh"></q-icon>
+                  {{weather.wind}}
+                </div>
+              </div>
               <div align="center" class="col">
                 <q-icon name="fas fa-tint"  color="white" size="3vh"></q-icon>
                 {{weather.humidity}}
@@ -42,14 +40,13 @@
                 <q-icon name="fas fa-cloud"  color="white" size="3vh"></q-icon>
                 {{weather.clouds}}
               </div>
+            </div>
+
           </div>
         </div>
       </div>
-
-
     </div>
-</div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -57,28 +54,28 @@
   import types from './../../store/types'
 
 
-    export default {
-      name: "WeatherTile",
+  export default {
+    name: "WeatherTile",
 
-      computed: {
-        ...mapState({
-          city: state => state.weatherStore.city,
-          weather: state => state.weatherStore.forecast
-        }),
-      },
-      methods:{
-        horiontalScrolling: function (event) {
-          if(event.deltaY > 0)
-            this.$refs.scroll.scrollLeft+=50
-          else
-            this.$refs.scroll.scrollLeft-=50
-        }
-
-      },
-     mounted() {
-       this.$store.dispatch(types.weather.updateDailyWeather,this.city)
+    computed: {
+      ...mapState({
+        city: state => state.weatherStore.city,
+        weather: state => state.weatherStore.forecast
+      }),
+    },
+    methods:{
+      horizontalScrolling: function (event) {
+        if(event.deltaY > 0)
+          this.$refs.scroll.scrollLeft+=50
+        else
+          this.$refs.scroll.scrollLeft-=50
       }
+
+    },
+    mounted() {
+      this.$store.dispatch(types.weather.updateDailyWeather,this.city)
     }
+  }
 </script>
 
 <style scoped>
@@ -92,9 +89,9 @@
     width: calc(40vw* (1/3));
     text-align: center;
   }
-.scroll{
-  width: calc( (40vw* (1/3))*41 );
-  height: 20vh;
-}
+  .scroll{
+    width: calc( (40vw* (1/3))*41 );
+    height: 20vh;
+  }
 
 </style>

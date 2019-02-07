08ecd8bf36289cfaf4d36c8a736e7adbd2fcc7d2
Bódi <axel3425@hotmail.com>
2019-02-07 16:18:39 +0100

    Weather-tile version 0.9.2c Code format

--- src/store/modules/weather/mutations.js
+++ src/store/modules/weather/mutations.js
@@ -3,40 +3,39 @@ import moment from 'moment'
 import {codeConverter} from "./WeatherCodes"
 
 const mutations= {
- [types.weather.setCurrentWeather]: (state,payload) => {
+  [types.weather.setCurrentWeather]: (state,payload) => {
     let now = moment();
-   state.forecast=[];
-   state.forecast.push({
-       id:0,
-       time : dayToString(now.isoWeekday()) + " " + now.hour() + ":00",
-       temp : Math.round(payload.main.temp),
-       cond : codeConverter(payload.weather[0].icon),
-       wind : Math.trunc(payload.wind.speed * 3.6),
-       description : payload.weather[0].description,
-       humidity : payload.main.humidity,
-       clouds : payload.clouds.all + "%"
-   });
- },
-
-[types.weather.setForecastWeather]: (state,payload) => {
-  let now = moment();
-  state.forecast=state.forecast.slice(0,1)
-
-  for (let i = 0; i < payload.length; ++i) {
-    now.add("3", "h");
+    state.forecast=[];
     state.forecast.push({
-      id:i+1,
-      time : dayToString(now.isoWeekday())+" "+ now.hour() + ":00",
-      temp: Math.round(payload[i].main.temp),
-      cond : codeConverter(payload[i].weather[0].icon),
-      wind : Math.trunc(payload[i].wind.speed * 3.6),
-      description : payload[i].weather[0].description,
-      humidity: payload[i].main.humidity,
-      clouds: payload[i].clouds.all + "%"
+      id:0,
+      time : dayToString(now.isoWeekday()) + " " + now.hour() + ":00",
+      temp : Math.round(payload.main.temp),
+      cond : codeConverter(payload.weather[0].icon),
+      wind : Math.trunc(payload.wind.speed * 3.6),
+      description : payload.weather[0].description,
+      humidity : payload.main.humidity,
+      clouds : payload.clouds.all + "%"
     });
-}
+  },
 
- },
+  [types.weather.setForecastWeather]: (state,payload) => {
+    let now = moment();
+    state.forecast=state.forecast.slice(0,1);
+
+    for (let i = 0; i < payload.length; ++i) {
+      now.add("3", "h");
+      state.forecast.push({
+        id:i+1,
+        time : dayToString(now.isoWeekday())+" "+ now.hour() + ":00",
+        temp: Math.round(payload[i].main.temp),
+        cond : codeConverter(payload[i].weather[0].icon),
+        wind : Math.trunc(payload[i].wind.speed * 3.6),
+        description : payload[i].weather[0].description,
+        humidity: payload[i].main.humidity,
+        clouds: payload[i].clouds.all + "%"
+      });
+    }
+  },
 };
 
 export default mutations
@@ -45,26 +44,26 @@ let dayToString= function f(day) {
   let dayString="";
   switch (day) {
     case 1:
-      dayString="Monday"
+      dayString="Monday";
       break;
     case 2:
-      dayString="Tuesday"
+      dayString="Tuesday";
       break;
     case 3:
-      dayString="Wednesday"
+      dayString="Wednesday";
       break;
     case 4:
-      dayString="Thursday"
+      dayString="Thursday";
       break;
     case 5:
-      dayString="Friday"
+      dayString="Friday";
       break;
     case 6:
-      dayString="Saturday"
+      dayString="Saturday";
       break;
     case 7:
-      dayString="Sunday"
+      dayString="Sunday";
       break;
   }
   return dayString;
-}
+};
--- src/store/modules/weather/weather.js
+++ src/store/modules/weather/weather.js
@@ -4,10 +4,7 @@ import actions from './actions'
 
 const state = {
   city:"Nyiregyhaza",
-
   forecast:[],
-  weekly: []
-
 };
 
 export default {

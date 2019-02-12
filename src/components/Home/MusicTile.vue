b9e4c3096ecd571e70d2a188c8017ff833d3b28a
Bódi <axel3425@hotmail.com>
2019-02-12 16:02:45 +0100

    Music-tile version 0.3.0 Vertical redesign and size unit fixes

--- src/components/Home/MusicTile.vue
+++ src/components/Home/MusicTile.vue
@@ -9,62 +9,59 @@
       </q-list>
     </q-context-menu>
 
+    <div class="row header items-center">
+      <div  class="col">
+        Now playing
+      </div>
+    </div>
 
-    <div class="row">
-      <div  class="col-4">
+    <div class="row items-center picture">
+      <div  class="col">
         <img src="https://img.purch.com/h/1400/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAwNS82NDQvb3JpZ2luYWwvbW9vbi13YXRjaGluZy1uaWdodC0xMDA5MTYtMDIuanBn"
              width="auto"
-             height="100">
-      </div>
-
-      <div class="col self-center">
-
-        <div class="row">
-          <div class="col align">
-            {{artist}}
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col align">
-            {{song}}
-          </div>
-        </div>
+             height="120">
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-10 ">
-        <div class="row align">
-          <div class="col">
-           <q-icon name="fas fa-backward"  color="white" size="8vh"></q-icon>
-          </div>
-          <div class="col" @click="PlayPause">
-            <q-icon   name= "fas fa-play"  color="white" size="8vh"></q-icon>
-          </div>
-          <div class="col">
-            <q-icon name= "fas fa-forward"  color="white" size="8vh"></q-icon>
-          </div>
-        </div>
-      </div>
-      <div class="col-2 self-center">
-        <div class="row">
-          <div class="col">
-            <q-icon name= "fas fa-redo"  color="white" size="2vh"></q-icon>
-          </div>
-          <div class="col">
-            <q-icon name= "fas fa-random"  color="white" size="2vh"></q-icon>
-          </div>
-        </div>
+    <div class="row items-center song-title">
+      <div class="col">
+        Welcome to the Jungle
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-11 align">
+    <div class="row items-center artist-and-album">
+      <div class="col">
+        Guns 'n Roses | Appetite for Destruction
+      </div>
+    </div>
+
+    <div class="row items-center progress-bar">
+      <div class="col">
         <q-Slider :min="min" :max="max" :step="1" />
       </div>
-      <div class="col-1">
-        <q-knob  class=""  :min="min" :max="max" size="3vw" color="primary"/>
+    </div>
+
+    <div class="row items-center controls">
+          <div class="col">
+            <q-icon name="fas fa-step-backward"  color="white" size="2em"></q-icon>
+          </div>
+          <div class="col" @click="PlayPause">
+            <q-icon   name= "fas fa-play"  color="white" size="3em"></q-icon>
+          </div>
+          <div class="col">
+            <q-icon name= "fas fa-step-forward"  color="white" size="2em"></q-icon>
+          </div>
+    </div>
+
+    <div class="row items-center volume-and-misc">
+      <div class="col-3">
+        <q-icon name= "fas fa-redo"  color="white" size="2vh"></q-icon>
+      </div>
+      <div class="col-6">
+        <q-Slider :min="min" :max="max" :step="1" />
+      </div>
+      <div class="col-3">
+        <q-icon name= "fas fa-random"  color="white" size="2vh"></q-icon>
       </div>
     </div>
   </div>
@@ -75,46 +72,69 @@
   import { mapState, mapActions } from 'vuex'
   import types from './../../store/types'
 
-    export default {
-      name: "MusicTile",
-      data () {
-        return {
-          min: 0,
-          max: 100,
-          showing: true
-        }
-      },
-        computed: {
-        ...mapState({
-          artist: state => state.musicStore.music.currentSong.artist,
-          song: state => state.musicStore.music.currentSong.title,
-          play: state => state.musicStore.player.play
-        })
-      },
-      methods:{
-        PlayPause: function () {
-          if(this.play === "pause")
-            this.$store.dispatch("musicStore/"+types.music.player.start);
-          else
-            this.$store.dispatch("musicStore/"+types.music.player.stop)
-        }
-      },
-      beforeMount() {
-        console.log(this.artist)
+  export default {
+    name: "MusicTile",
+    data () {
+      return {
+        min: 0,
+        max: 100,
+        showing: true
       }
+    },
+    computed: {
+      ...mapState({
+        artist: state => state.musicStore.music.currentSong.artist,
+        song: state => state.musicStore.music.currentSong.title,
+        play: state => state.musicStore.player.play
+      })
+    },
+    methods:{
+      PlayPause: function () {
+        if(this.play === "pause")
+          this.$store.dispatch("musicStore/"+types.music.player.start);
+        else
+          this.$store.dispatch("musicStore/"+types.music.player.stop)
+      }
+    },
+    beforeMount() {
+      console.log(this.artist)
     }
+  }
 </script>
 
 <style scoped>
 
-  .align{
-    text-align: center;
-  }
   .background{
-   background-color: rgba(12, 34, 12, 0.58);
+    background-color: rgba(12, 34, 12, 0.58);
   }
   .size{
-    width: 30vh;
-    height: 50vh;
+    width: 15em;
+    height: 25em;
   }
+
+  .header{
+    height: 10%;
+  }
+  .picture{
+    height: 30%;
+  }
+  .song-title{
+    font-size: 1.2em;
+    height: 10%;
+  }
+  .artist-and-album{
+    font-size: 0.8em;
+    height: 10%;
+  }
+  .progress-bar{
+   height: 10%;
+    width: 95%;
+  }
+  .controls{
+    height: 20%;
+  }
+  .volume-and-misc{
+    height: 10%;
+  }
+
 </style>

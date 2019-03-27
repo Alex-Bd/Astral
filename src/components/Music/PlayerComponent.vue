1283f6cbf4cc02bdb8e16f4d7e4235a0563d2a9e
Alex-Bodi <alex.bodi@hotmail.com>
2019-03-27 17:26:30 +0100

    Created Music List UI for Music Player page

--- src/components/Music/PlayerComponent.vue
+++ src/components/Music/PlayerComponent.vue
@@ -1,15 +1,44 @@
 <template>
   <q-page>
-    <div>asd</div>
+    <div class="row">
+      <div class="col">
+        <q-list bordered dark>
+          <q-item clickable v-ripple :active="active">
+            <q-item-section side>1</q-item-section>
+            <q-item-section>Legends Never Die</q-item-section>
+            <q-item-section>Season 2017</q-item-section>
+            <q-item-section>League of Legends</q-item-section>
+            <q-item-section side>3:17</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+      <div class="col-3">
+        <q-list bordered dark>
+          <q-item clickable v-ripple :active="active">
+            <q-item-section>
+              <q-img
+                src="https://i.imgur.com/eukJn7L.jpg"
+                class="album-img"
+                :ratio="1"
+              ></q-img>
+            </q-item-section>
+            <q-item-section>Legends Never Die</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-    export default {
-        name: "PlayerComponent"
-    }
+export default {
+  name: "PlayerComponent"
+};
 </script>
 
 <style scoped lang="stylus">
 
+.album-img
+  height 10em
+  width 10em
 </style>

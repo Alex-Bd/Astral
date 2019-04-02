335c32864cccecae09325b885620d3cea94512f1
Alex-Bodi <alex.bodi@hotmail.com>
2019-04-02 13:07:35 +0200

    Fix weather API domain name

--- src/store/modules/weather/actions.js
+++ src/store/modules/weather/actions.js
@@ -4,13 +4,13 @@ import axios from "axios";
 const actions = {
   [types.weather.updateDailyWeather]: ({ commit }, payload) => {
     axios
-      .get("http://localhost:8070/Weather?city=" + payload)
+      .get(types.domain+"/Weather?city=" + payload)
       .then(function(response) {
         commit(types.weather.setCurrentWeather, response.data);
       });
 
     axios
-      .get("http://localhost:8070/Forecast?city=" + payload)
+      .get(types.domain+"/Forecast?city=" + payload)
       .then(function(response) {
         commit(types.weather.setForecastWeather, response.data.list);
       });
